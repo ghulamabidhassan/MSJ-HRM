ecacdb5b817e58175e6ec47cbe8ad3e9d49be365
Abid <ghulamabidhassan@gmail.com>
2023-04-29 16:52:31 +0400

    fetch test

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,21 +1,43 @@
-import { nanoid } from "nanoid";
 import dotenv from "dotenv";
 import mysql from "mysql2/promise";
+import { nanoid } from "nanoid";
 
 dotenv.config();
-const id = nanoid(8);
 
 export const handler = async (event, context) => {
   const pool = await mysql.createPool(process.env.DATABASE_URL);
-  const [result] = await pool.query("select email from abidtable");
+  const [result] = await pool.query("select * from abidtable");
+  const method = event.httpMethod;
+  const ref = event.headers.referer;
+
+  if (method === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ref,
+        UId: nanoid(10),
+        type: method,
+        data: result,
+        status: "Success",
+        statusCode: 200,
+        event,
+      }),
+    };
+  }
+
+  if (method === "PUT") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ref,
+        UId: nanoid(10),
+        type: method,
+      }),
+    };
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      data: result,
-      id,
-      status: "Success",
-      statusCode: 200,
-    }),
+    body: JSON.stringify({ msg: "ONLY GET AND PUT", ref }),
   };
 };
