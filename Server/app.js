1cdd6bfa5256ab5eeea47a52a2576880f67eb983
Abid <ghulamabidhassan@gmail.com>
2023-05-11 22:36:38 +0400

    recommit

--- Server/app.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", () => {
-  location.replace("/.netlify/functions/1-hello");
-});
--- Server/data.js
+++
@@ -1,4 +0,0 @@
-export const data = [
-  { id: 1, msg: "Hello" },
-  { id: 2, msg: "World" },
-];
